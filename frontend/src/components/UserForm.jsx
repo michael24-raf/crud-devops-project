f69f0e6c1c447a43780acd56683a8fdca9b63522
michael24-raf <rafanoharanamickael1723@gmail.com>
2026-01-11 08:06:15 +0300

    maj 6

--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -99,7 +99,7 @@ const UserForm = ({ onSubmit, userToEdit, onCancelEdit }) => {
       
       <form onSubmit={handleSubmit} className="user-form">
         <div className="form-group">
-          <label htmlFor="name">Prenom *</label>
+          <label htmlFor="name">Nom *</label>
           <input
             type="text"
             id="name"
